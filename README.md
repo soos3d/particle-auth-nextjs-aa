3b1fc005ebd4eece3f70c22d29de1ccdd1b4dc3b
Soos3D <99700157+soos3d@users.noreply.github.com>
2024-07-10 18:53:28 -0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
-# particle-auth-nextjs-web3js
- Next.js starter using Particle Network's Auth Core SDK and Web3.js
+<div align="center">
+  <a href="https://particle.network/">
+    <img src="https://i.imgur.com/xmdzXU4.png" />
+  </a>
+  <h3>
+ @particle-network/auth-core Demo Application 
+  </h3>
+</div>
+
+# Particle Auth, Account Abstraction SDK, Next.js, & ethers V6
+
+⚡️ Basic demo application using `@particle-network/auth-core` and `@particle-network/aa` to initiate social login and send transactions via an account abstraction smart account. 
+
+This is a lower-level library and powers `@particle-network/auth-core-modal` - most additional functionality beyond the aforementioned (login and transaction execution) will need to be built by the developer implementing this library.
+
+This app allows you to log in using social logins and interact with the Ethereum Sepolia and Base Sepolia testnets by displaying account information and sending a transfer transaction to an address you can input in the UI. The user can select to send a gasless transaction or pay gas with the native token.
+
+It also showcases how to sign simple messages.
+
+> The Next application is within the `particle-auth-aa` directory.
+
+> 🛠️ Try the demo: [Particle Auth Next.js demo](https://particle-next-starter.vercel.app/)
+
+Built using:
+
+- **Next.js**
+- **Particle Auth Core**
+- **Particle AA SDK**
+- **ethers.js V6.x.x**
+- **TypeScript**
+- **`src/` directory**
+- **App router**
+- **Tailwind CSS**
+
+## 🔑 Particle Auth Core
+
+Particle Auth Core, a component of Particle Network's Wallet-as-a-Service, enables seamless onboarding to an application-embedded MPC-TSS/AA wallet facilitated by social login, such as Google, GitHub, email, phone number, etc. - as an alternative to Particle Auth, the Auth Core SDK comes with more control over the modal itself, application-embedded popups rather than redirects, and so on.
+
+👉 Learn more about [Particle Auth](https://developers.particle.network/docs/building-with-particle-auth).
+
+## 🪪 Account Abstraction SDK
+
+Particle Network natively supports and facilitates end-to-end utilization of ERC-4337 account abstraction. This is primarily done through the account abstraction SDK, capable of constructing, sponsoring, and sending UserOperations, alongside deploying smart accounts, retrieving fee quotes, and other key functions.
+
+> On testnet, every gasless transaction is sponsored automatically. On mainnet you'll need to deposit USDT in the Paymaster.
+
+👉 Learn more about the [Particle AA SDK](https://developers.particle.network/docs/aa-web-quickstart).
+
+***
+
+👉 Learn more about [Particle Network](https://particle.network).
+
+## Deploy with Vercel
+
+<p align="center">
+<a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsoos3d%2Fparticle-next-starter%2Ftree%2Fmain%2Fparticle-next-starter&env=NEXT_PUBLIC_PROJECT_ID,NEXT_PUBLIC_CLIENT_KEY,NEXT_PUBLIC_APP_ID"><img src="https://vercel.com/button" alt="Deploy with Vercel"/></a>
+</p>
+
+## 🛠️ Quickstart
+
+### Clone this repository
+```
+git clone https://github.com/soos3d/particle-auth-nextjs-ethers.git
+```
+
+### Move into the app directory
+
+```sh
+cd particle-next-starter
+```
+
+### Install dependencies
+
+```sh
+yarn install
+```
+
+Or
+
+```sh
+npm install
+```
+
+### Set environment variables
+This project requires several keys from Particle Network to be defined in `.env`. The following should be defined:
+- `NEXT_PUBLIC_PROJECT_ID`, the ID of the corresponding application in your [Particle Network dashboard](https://dashboard.particle.network/#/applications).
+- `NEXT_PUBLIC_CLIENT_KEY`, the ID of the corresponding project in your [Particle Network dashboard](https://dashboard.particle.network/#/applications).
+-  `NEXT_PUBLIC_APP_ID`, the client key of the corresponding project in your [Particle Network dashboard](https://dashboard.particle.network/#/applications).
+
+### Start the project
+```sh
+npm run dev
+```
+
+Or
+
+```sh
+yarn dev
+```
+
+## Development
+
+Particle Auth config is in `src/app/layout.tsx`. 
+
+List of available social logins:
+
+```sh
+{
+  email: 'email',
+  phone: 'phone',
+  facebook: 'facebook',
+  google: 'google',
+  apple: 'apple',
+  twitter: 'twitter',
+  discord: 'discord',
+  github: 'github',
+  twitch: 'twitch',
+  microsoft: 'microsoft',
+  linkedin: 'linkedin',
+  jwt: 'jwt'
+}
+```
---
+++ particle-auth-aa/src/app/page.tsx
@@ -0,0 +1,304 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import type { NextPage } from "next";
+
+// Import Particle Auth hooks and provider
+import {
+  useEthereum,
+  useConnect,
+  useAuthCore,
+} from "@particle-network/auth-core-modal";
+import {
+  AAWrapProvider,
+  SendTransactionMode,
+  SmartAccount,
+} from "@particle-network/aa";
+import { BaseSepolia, EthereumSepolia } from "@particle-network/chains";
+import { ethers } from "ethers";
+
+// UI component to display links to the Particle sites
+import LinksGrid from "./components/Links";
+import Header from "./components/Header";
+
+// Import the utility functions
+import { formatBalance, truncateAddress } from "./utils/utils";
+
+// Create a wrapper to adapt AAWrapProvider to Eip1193Provider
+class Eip1193ProviderWrapper {
+  private provider: AAWrapProvider;
+
+  constructor(provider: AAWrapProvider) {
+    this.provider = provider;
+  }
+
+  async request(request: {
+    method: string;
+    params?: any[] | Record<string, any>;
+  }) {
+    return this.provider.request({
+      method: request.method,
+      params: Array.isArray(request.params) ? request.params : [request.params],
+    });
+  }
+}
+
+const Home: NextPage = () => {
+  // Hooks to manage logins, data display, and transactions
+  const { connect, disconnect, connectionStatus } = useConnect();
+  const { provider, chainInfo, signMessage } = useEthereum();
+  const { userInfo } = useAuthCore();
+
+  const [balance, setBalance] = useState<string>(""); // states for fetching and display the balance
+  const [recipientAddress, setRecipientAddress] = useState<string>(""); // states to get the address to send tokens to from the UI
+  const [selectedProvider, setSelectedProvider] = useState<string>("ethers"); // states to handle which providers signs the message
+  const [selectedMode, setSelectedMode] = useState<SendTransactionMode>(
+    SendTransactionMode.Gasless
+  ); // state to handle the selected transaction mode. Gasless by default
+  const [address, setAddress] = useState<string>(""); // states to handle the address of the smart account
+
+  // Set up and configure the smart account
+  const smartAccount = new SmartAccount(provider, {
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID || "",
+    clientKey: process.env.NEXT_PUBLIC_CLIENT_KEY || "",
+    appId: process.env.NEXT_PUBLIC_APP_ID || "",
+    aaOptions: {
+      accountContracts: {
+        SIMPLE: [
+          {
+            version: "1.0.0", // SIMPLE only allows 1.0.0
+            chainIds: [EthereumSepolia.id, BaseSepolia.id],
+          },
+        ],
+      },
+    },
+  });
+
+  smartAccount.setSmartAccountContract({ name: "SIMPLE", version: "1.0.0" });
+
+  // Function to create ethers provider based on selected mode
+  const createEthersProvider = (mode: SendTransactionMode) => {
+    return new ethers.BrowserProvider(
+      new Eip1193ProviderWrapper(new AAWrapProvider(smartAccount, mode)),
+      "any"
+    );
+  };
+
+  // Initialize the ethers provider
+  const [ethersProvider, setEthersProvider] = useState(() =>
+    createEthersProvider(selectedMode)
+  );
+
+  // Update ethers provider when selectedMode changes
+  useEffect(() => {
+    setEthersProvider(createEthersProvider(selectedMode));
+  }, [selectedMode]);
+
+  // Fetch the balance when userInfo or chainInfo changes
+  useEffect(() => {
+    if (userInfo) {
+      fetchBalance();
+    }
+  }, [userInfo, chainInfo]);
+
+  // Fetch the user's balance in Ether
+  const fetchBalance = async () => {
+    try {
+      // Get the smart account address
+      const address = await smartAccount.getAddress();
+      const balanceResponse = await ethersProvider.getBalance(address);
+      const balanceInEther = ethers.formatEther(balanceResponse);
+
+      // Format the balance using the utility function
+      const fixedBalance = formatBalance(balanceInEther);
+
+      setAddress(address);
+      setBalance(fixedBalance);
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+    }
+  };
+
+  // Handle user login
+  const handleLogin = async () => {
+    if (!userInfo) {
+      await connect({});
+    }
+  };
+
+  // Handle user disconnect
+  const handleDisconnect = async () => {
+    try {
+      await disconnect();
+    } catch (error) {
+      console.error("Error disconnecting:", error);
+    }
+  };
+
+  // Execute an Ethereum transaction
+  // Simple transfer in this example
+  const executeTxEvm = async () => {
+    const signer = await ethersProvider.getSigner();
+    const tx = {
+      to: recipientAddress,
+      value: ethers.parseEther("0.01"),
+      data: "0x",
+    };
+
+    try {
+      const txResponse = await signer.sendTransaction(tx);
+      const txReceipt = await txResponse.wait();
+      if (txReceipt) {
+        alert(`Transaction Successful. Transaction Hash: ${txReceipt.hash}`);
+      } else {
+        console.error("Transaction receipt is null");
+      }
+    } catch (error) {
+      console.error("Error executing EVM transaction:", error);
+    }
+  };
+
+  // Sign a message using ethers as provider
+  const signMessageEthers = async () => {
+    const signer = await ethersProvider.getSigner();
+    const signerAddress = await signer.getAddress();
+
+    const message = "Gm Particle! Signing with ethers.";
+
+    try {
+      const result = await signMessage(message);
+      alert(`Signed Message: ${result} by address ${signerAddress}.`);
+    } catch (error: any) {
+      // This is how you can display errors to the user
+      alert(`Error with code ${error.code}: ${error.message}`);
+      console.error("personal_sign", error);
+    }
+  };
+
+  // Sign message using Particle Auth Natively
+  const signMessageParticle = async () => {
+    const message = "Gm Particle! Signing with Particle Auth.";
+
+    try {
+      const smartAddress = await smartAccount.getAddress();
+      const result = await signMessage(message);
+      alert(`Signed Message: ${result} by address ${smartAddress}.`);
+    } catch (error: any) {
+      // This is how you can display errors to the user
+      alert(`Error with code ${error.code}: ${error.message}`);
+      console.error("personal_sign", error);
+    }
+  };
+
+  // The UI
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-between p-8 bg-black text-white">
+      <Header />
+      <main className="flex-grow flex flex-col items-center justify-center w-full max-w-6xl mx-auto">
+        {!userInfo ? (
+          <div className="login-section">
+            <button
+              className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 shadow-lg"
+              onClick={handleLogin}
+            >
+              Sign in with Particle
+            </button>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+            <div className="border border-purple-500 p-6 rounded-lg">
+              <h2 className="text-2xl font-bold mb-2 text-white">
+                Accounts info
+              </h2>
+              <h2 className="text-lg font-semibold mb-2 text-white">
+                Name: {userInfo.name}
+              </h2>
+              <h2 className="text-lg font-semibold mb-2 text-white">
+                Status: {connectionStatus}
+              </h2>
+
+              <h2 className="text-lg font-semibold mb-2 text-white">
+                Address: <code>{truncateAddress(address || "")}</code>
+              </h2>
+              <h3 className="text-lg mb-2 text-gray-400">
+                Chain: {chainInfo.fullname}
+              </h3>
+              <h3 className="text-lg font-semibold text-purple-400">
+                Balance: {balance} {chainInfo.nativeCurrency.symbol}
+              </h3>
+              <div>
+                <button
+                  className="mt-4 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 shadow-lg"
+                  onClick={handleDisconnect}
+                >
+                  Disconnect
+                </button>
+              </div>
+            </div>
+            <div className="border border-purple-500 p-6 rounded-lg">
+              <h2 className="text-2xl font-bold mb-2 text-white">
+                Send a transaction
+              </h2>
+              <h2 className="text-lg">Send 0.01 ETH</h2>
+              <input
+                type="text"
+                placeholder="Recipient Address"
+                value={recipientAddress}
+                onChange={(e) => setRecipientAddress(e.target.value)}
+                className="mt-4 p-2 w-full rounded border border-gray-700 bg-gray-900 text-white focus:outline-none focus:ring-2 focus:ring-purple-400"
+              />
+              <select
+                value={selectedMode}
+                onChange={(e) =>
+                  setSelectedMode(
+                    parseInt(e.target.value) as SendTransactionMode
+                  )
+                }
+                className="mt-4 p-2 w-full rounded border border-gray-700 bg-gray-900 text-white focus:outline-none focus:ring-2 focus:ring-purple-400"
+              >
+                <option value={SendTransactionMode.Gasless}>Gasless</option>
+                <option value={SendTransactionMode.UserPaidNative}>
+                  User Paid Native
+                </option>
+                <option value={SendTransactionMode.UserSelect}>
+                  User Select
+                </option>
+              </select>
+              <button
+                className="mt-4 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 shadow-lg"
+                onClick={executeTxEvm}
+                disabled={!recipientAddress}
+              >
+                Send 0.01 ETH
+              </button>
+            </div>
+            <div className="border border-purple-500 p-6 rounded-lg">
+              <h2 className="text-2xl font-bold mb-2">Sign a Message</h2>
+              <p className="text-lf">Pick a provider to sign with:</p>
+              <select
+                value={selectedProvider}
+                onChange={(e) => setSelectedProvider(e.target.value)}
+                className="mt-4 p-2 w-full rounded border border-gray-700 bg-gray-900 text-white focus:outline-none focus:ring-2 focus:ring-purple-400"
+              >
+                <option value="ethers">Ethers Provider</option>
+                <option value="particle">Particle Auth</option>
+              </select>
+              <button
+                className="mt-4 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 shadow-lg"
+                onClick={
+                  selectedProvider === "ethers"
+                    ? signMessageEthers
+                    : signMessageParticle
+                }
+              >
+                Sign Message
+              </button>
+            </div>
+          </div>
+        )}
+        <LinksGrid />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
